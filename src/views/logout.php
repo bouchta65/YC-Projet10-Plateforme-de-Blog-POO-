8c8c4a308e8017e129eabca7bfdada2ae7322470
bouchta65 <bouchtamohamed01@gmail.com>
2025-01-07 23:22:57 +0100

    Add logout functionality to properly destroy user session.

--- src/views/logout.php
+++ src/views/logout.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+session_start(); 
+include '../classes/User.php';
+$user = unserialize($_SESSION['user_obj']);
+$user->logout();
+exit();
+?>
